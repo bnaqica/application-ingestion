637baa86eb0fde48047d7bc9881f0fbe3964ed7f
bnaqica <bnaqica@ysgco.com>
2023-02-13 07:35:22 -0700

    Initial commit of service structure

---
+++ src/main/java/com/bnaqica/applicationinjestion/ApplicationInjestionApplication.java
@@ -0,0 +1,13 @@
+package com.bnaqica.applicationinjestion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationInjestionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationInjestionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bnaqica/applicationinjestion/ApplicationInjestionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bnaqica.applicationinjestion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationInjestionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
